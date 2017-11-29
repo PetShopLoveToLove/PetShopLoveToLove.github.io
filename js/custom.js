54c1059ad40c781fcd02023ec47f0b3b7aafd508
igor <igby.veko@gmail.com>
2017-11-29 13:41:50 +0100

    poslednja verzija

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,5 @@
 
-var $ = function (selector) {
-    return document.querySelector(selector);
-  };
+var $ = selector => document.querySelector(selector);
 
 // google map
 function initMap() {
@@ -36,11 +34,12 @@ var imgAnim4 = $('.pet-img-anim4');
 var spanAnim1 = $('.pet-ex-span-anim1');
 var spanAnim2 = $('.pet-ex-span-anim2');
 
-function anim() {
+function animLogoSideBarOnama() {
 
-  // 'hidden nav' animation
-  if (window.pageYOffset > 15) {
+  // 'lg screen' hidden nav' animation
+  if (window.pageYOffset > 50) {
     logo.style.marginTop = '-150px';
+    logo.style.opacity = '0.3';
     logo.style.transition = '1s';
     petNav.style.height = '50px';
     petNav.style.transition = '1s';
@@ -48,42 +47,47 @@ function anim() {
 
   else {
     logo.style.marginTop = '0';
+    logo.style.opacity = '1';
     logo.style.transition = '1s';
     petNav.style.height = '103px';
     petNav.style.transition = '1s';
   }
-  
-  // 'side bar' animation
+   
+  // 'lg screen side bar' animation
   if (window.pageYOffset > 15) {
     sideBarHolder.style.left = '-200px';
     sideBarHolder.style.transition = '1s';
 
+    sideBarHolderHidden.style.opacity = '1';
     sideBarHolderHidden.style.marginLeft = 0;
-    sideBarHolderHidden.style.transition = '1.5s';
+    sideBarHolderHidden.style.transition = '1.8s';
   }
   else {
     sideBarHolder.style.left = '0';
     sideBarHolder.style.transition = '1.5s';
 
-    sideBarHolderHidden.style.marginLeft = '-43px';
+    sideBarHolderHidden.style.opacity = '0';
+    sideBarHolderHidden.style.marginLeft = '-53px';
     sideBarHolderHidden.style.transition = '1s';
   }
   
   //'o nama' animation
-  if (window.pageYOffset > 725) { 
-    imgAnim1.style.height = '302px';
+  if (window.pageYOffset > 550) { 
+    imgAnim1.style.height = '100%';
+    imgAnim1.style.width = '100%';
     imgAnim1.style.transition = '1s';
     imgAnim1.style.bottom = '0';
 
-    imgAnim4.style.height = '302px';
+    imgAnim4.style.height = '100%';
+    imgAnim4.style.width = '100%';
     imgAnim4.style.transition = '1s';
     imgAnim4.style.top = '0';
 
-    txtAnim2.style.height = '302px';
+    txtAnim2.style.height = '100%';
     txtAnim2.style.transition = '1s';
     txtAnim2.style.bottom = '0';
 
-    txtAnim3.style.height = '302px';
+    txtAnim3.style.height = '100%';
     txtAnim3.style.transition = '1s';
     txtAnim3.style.top = '0';
 
@@ -95,7 +99,39 @@ function anim() {
   }
 };
 
-document.addEventListener('scroll', anim);
+var sideBarNotOnama = $('.pet-side-bar-holder-hidden-not_o_nama');
+
+function animHiddenSideBarNotOnamaPages() {
+
+// 'hidden not_o_nama nav' animation
+  if (window.pageYOffset > 100) {
+    sideBarNotOnama.style.marginLeft = '0';
+    sideBarNotOnama.style.transition = '1s';
+  }
+  else {
+    sideBarNotOnama.style.marginLeft = '-60px';
+  }
+};
+
+var hiddenLogo = $('.pet-hidden-logo-img');
+
+function hiddenLogoShow() {
+  
+  if (window.pageYOffset > 150) {
+    hiddenLogo.style.opacity = '1';
+    hiddenLogo.style.transition = '1.5s';
+  }
+  else {
+    hiddenLogo.style.opacity = '0';
+    hiddenLogo.style.transition = '1s';
+  }  
+};
+
+
+document.addEventListener('scroll', animLogoSideBarOnama);
+document.addEventListener('scroll', hiddenLogoShow);
+document.addEventListener('scroll', animHiddenSideBarNotOnamaPages);
+
 
 
 
